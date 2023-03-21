bfb5c484d16fc00c1fbfc5d0a13b6d8daeea688f
martinManzanares <113073247+martinManzanares@users.noreply.github.com>
2023-03-21 15:00:48 -0600

    Add files via upload
    interfaces - primer avance.

---
+++ app/src/main/java/martin/rivera/packsnpaws/MainActivity.kt
@@ -0,0 +1,11 @@
+package martin.rivera.packsnpaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
