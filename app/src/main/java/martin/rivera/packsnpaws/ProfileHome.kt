d31e2476a0ed2f97e7996e352425ef17b848cd48
martinManzanares <113073247+martinManzanares@users.noreply.github.com>
2023-03-29 11:05:46 -0600

    update-2
    Retro de la primera revision

---
+++ app/src/main/java/martin/rivera/packsnpaws/ProfileHome.kt
@@ -0,0 +1,11 @@
+package martin.rivera.packsnpaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_home)
+    }
+}
