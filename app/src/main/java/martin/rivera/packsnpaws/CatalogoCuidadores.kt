a4418e889ddfbe524ad801c6de305eb9756502d1
martinManzanares <113073247+martinManzanares@users.noreply.github.com>
2023-04-17 14:28:13 -0600

    update-3-abril

---
+++ app/src/main/java/martin/rivera/packsnpaws/CatalogoCuidadores.kt
@@ -0,0 +1,11 @@
+package martin.rivera.packsnpaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CatalogoCuidadores : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalogo_cuidadores)
+    }
+}
