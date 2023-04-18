99397ab286d1514c9af854cfe14ff24b9bf16111
martinManzanares <113073247+martinManzanares@users.noreply.github.com>
2023-04-18 16:54:46 -0600

    update-18-abr

---
+++ main/java/martin/rivera/packsnpaws/Chats.kt
@@ -0,0 +1,11 @@
+package martin.rivera.packsnpaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chats : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chats)
+    }
+}
